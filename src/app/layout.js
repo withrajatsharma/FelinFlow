d50cd5e43a6baef308f28ee97e03c2be784c7e19
RajatSharma <withrajatsharma@gmail.com>
2024-04-24 21:55:20 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,14 +18,14 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-  title: "Hayasaka",
+  title: "Groovezilla",
   description: "Music streaming app",
   image:
     "https://res.cloudinary.com/dbr73rpz9/image/upload/v1690380865/images/logo-color_noktgr.png",
   url: "https://hayasaka.vercel.app",
   type: "website",
   icons: [{ rel: "icon", url: Favicon.src }],
-  site_name: "Hayasaka",
+  site_name: "Grovezilla",
   manifest: "/manifest.json",
 };
 
